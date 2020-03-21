3a18b229bf86a6290ae6c8b865cc41500bb455cc
Adrian Block <a.block@brickfire.eu>
2020-03-21 23:22:15 +0100

    commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,8 +4,18 @@
             <h1 class="display-4">Login</h1>
         </div>
         <div class="container">
-
-            <p>{{ info }}</p>
+            <div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="E-Mail-Adresse">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Passwort</label>
+                    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Passwort">
+                </div>
+                <button v-on:click="login" class="btn btn-primary">Login</button>
+            </div>
+            <p v-if="res1">Eigeloggt mit Token: {{ res1.data.token }}</p>
         </div>
     </div>
 </template>
@@ -15,20 +25,26 @@
     export default {
         data() {
             return {
-                info: null,
+                res1: null,
+                email: null,
+                password: null,
+                token: null,
             }
         },
-        mounted () {
-            axios
-                .post('https://apibt.brickfire.eu/rest/json/person/login', {
-                    "email": "max9@test.com", "password": "1234"
-                })
-                .then(response => (this.info = response))
-                .catch(error => {
-                    console.log(error)
-                    this.errored = true
-                })
-
+        methods: {
+            login: function () {
+                axios
+                    .post('https://apibt.brickfire.eu/rest/json/person/login', {
+                        "email": this.email, "password": this.password
+                    })
+                    .then(response => (this.res1 = response))
+                    .catch(error => {
+                        console.log(error)
+                        this.errored = true
+                    });
+                this.token = this.res1.data.token;
+            }
         },
+
     }
 </script>
