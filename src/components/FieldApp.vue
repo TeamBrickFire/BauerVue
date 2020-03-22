a2c3a907d7e06f8668e5777e98f70ad24a8f6895
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2020-03-22 16:08:47 +0100

    Sidbar Anfang

--- src/components/FieldApp.vue
+++ src/components/FieldApp.vue
@@ -4,16 +4,33 @@
             <h1 class="display-4">BauerApp</h1>
         </div>
         <div class="container">
-            <h3>Feld von dir</h3>
-            <h4>Feld aus Ansicht des Bauers</h4>
-            <FarmFields fieldID="123" readonly="false"/>
+            <FieldSelect class="sidebar"/>
+            <div>
+                <h3>Feld von dir</h3>
+                <h4>Feld aus Ansicht des Bauers</h4>
+                <FarmFields fieldID="123" readonly="false"/>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
     import FarmFields from "@/components/FarmFields";
+    import FieldSelect from "./FieldSelect";
     export default {
-        components: {FarmFields},
+        components: {FieldSelect, FarmFields},
     }
 </script>
+
+<style scoped>
+    .container{
+        position: relative;
+        left: 100px;
+    }
+
+    .sidebar {
+        position: absolute;
+        left: -425px;
+    }
+</style>
